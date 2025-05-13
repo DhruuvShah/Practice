051e689aaefe9525c08f226ce0d0d969653c9dfc
Dhruv271006011006 <dhruv27shah@gmail.com>
2025-05-13 08:03:38 +0530

    Added GTA - 6 Project and DSA Questions

---
+++ DSA/script.js
@@ -0,0 +1,29 @@
+// 1. Movie Rating
+
+// let rating = Number(prompt('Enter the rating of movie'));
+// let movie = prompt('Enter the movie name');
+
+// if(rating >= 0.0 && rating <= 2.0){
+//     console.log(`${movie} is Flop movie`)
+// }
+// else if(rating >= 2.1 && rating <= 3.4){
+//     console.log(`${movie} is Semi-hit movie`)
+// }
+// else if(rating >= 3.5 && rating <= 4.5){
+//     console.log(`${movie} is Hit movie`)
+// }
+// else{
+//     console.log(`${movie} is Super Hit movie`)
+// }
+
+// 2. Profile based program
+
+let gender = prompt('Enter you gender');
+let service = Number('Enter year of service');
+let qualification = prompt('Enter you qualification')
+
+if(male){
+   if(service >= 10 && qualification ){
+
+   }
+}
